596155e23414a63fb86c929fded2c23367b2ed07
gitsparthiv <168458757+gitsparthiv@users.noreply.github.com>
2025-07-29 22:15:00 +0530

    Create diagonal_difference.cpp

---
+++ HACKERRANK/diagonal_difference.cpp
@@ -0,0 +1,161 @@
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+// For example, the square matrix  is shown below:
+
+// 1 2 3
+// 4 5 6
+// 9 8 9  
+// The left-to-right diagonal = .
+// The right-to-left diagonal = .
+// Their absolute difference is .
+
+// Function description
+
+// Complete the  function with the following parameter:
+
+// : a 2-D array of integers
+// Return
+
+// : the absolute difference in sums along the diagonals
+// Input Format
+
+// The first line contains a single integer, , the number of rows and columns in the square matrix .
+// Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+// Constraints
+
+// Sample Input
+
+// STDIN      Function
+// -----      --------
+// 3           arr[][] sizes n = 3, m = 3
+// 11 2 4     arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+// 4 5 6
+// 10 8 -12
+// Sample Output
+
+// 15
+// Explanation
+
+// The primary diagonal is:
+
+// 11
+//    5
+//      -12
+// Sum across the primary diagonal: .
+
+// The secondary diagonal is:
+
+//      4
+//    5
+// 10
+// Sum across the secondary diagonal: 
+// Difference: 
+
+// Note: |x| is the absolute value of x.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int diagonalDifference(vector<vector<int>> arr) {
+int left = 0;
+int right = 0;
+for(int i = 0; i < arr.size(); i++)
+{
+    left = left + arr[i][i];
+    right = right + arr[i][arr.size()- i - 1];
+}
+if(left < right)
+{
+    return right - left;
+}
+else{
+    return left - right;
+}
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < n; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
