124874115a818da140cdaf16ae4e08deb97c4ee5
gitsparthiv <168458757+gitsparthiv@users.noreply.github.com>
2026-03-25 00:51:56 +0530

    Update binary_search.cpp

--- LEETCODE/binary_search.cpp
+++ LEETCODE/binary_search.cpp
@@ -3,16 +3,20 @@ public:
     int search(vector<int>& nums, int target) {
         int i = 0;
         int n =nums.size();
-  while(i < n){
-    if(nums[i] == target)
-    {
-        return i;
+int low = 0;
+int high = n - 1;
+while( low <= high ){
+    int mid = (low + high)/2;
+    if(nums[mid] == target){
+        return mid;
     }
-    else
-    {
-        i++;
-    }
-    }
-    return -1;
-    }
+    else if(target < nums[mid]){
+            high = mid - 1;
+        }
+        else if(target > nums[mid]){
+            low = mid + 1;
+        }
+}
+return -1;
+}
 };
