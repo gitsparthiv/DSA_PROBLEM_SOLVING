5d733561e6545df33f7ea0c36f8e30018c6bf67b
gitsparthiv <168458757+gitsparthiv@users.noreply.github.com>
2026-05-02 21:11:40 +0530

    Create Maximum_Depth_of_Binary_Tree.cpp

---
+++ LEETCODE/Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxi = 0;
+
+    int findMax(TreeNode* node) {
+        if (node == NULL) return 0;
+
+        int lh = findMax(node->left);
+        int rh = findMax(node->right);
+
+        // update diameter
+        maxi = max(maxi, lh + rh);
+
+        // return height
+        return 1 + max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        findMax(root);
+        return maxi;
+    }
+};
